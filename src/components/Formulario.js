f7f0433da50ea94eed39d52f1533c83cd16e4411
JoseIgnacioTapia <josignaciot@gmail.com>
2021-06-08 16:18:42 -0300

    Validating Form and Error Message

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Error from './Error';
 
 const Formulario = () => {
+
+  // Guardar categoría buscada
+  const [termino, guardarTermino] = useState('');
+  const [error, guardarError] = useState(false);
+
+  // Validar formulario y enviar el termino al componente principal
+  const validarFormulario = e => {
+    e.preventDefault();
+    // Validar
+    if (termino.trim() === '') {
+      guardarError(true);
+      return;
+    }
+    guardarError(false);
+
+    // Enviar el termino a App.js
+  }
+
+
   return (
-    <form>
+    <form
+      onSubmit={validarFormulario}
+    >
       <div className="row">
         <div className="form-group col-md-8">
           <input 
             type="text"
             className="form-control form-control-lg"
             placeholder="Busca una imagen, ejemplo: futbol o café"
+            onChange={e => guardarTermino(e.target.value)}
           />
         </div>
         <div className="form-group col-md-4">
@@ -19,6 +42,8 @@ const Formulario = () => {
           />
         </div>
       </div>
+
+      { error ? <Error mensaje="El campo no puede estar vacío" /> : null }
     </form> 
   );
 }
